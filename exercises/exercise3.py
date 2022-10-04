7606b38d0e19d52db29ce65d29012937f35f2323
Francisco Urquiza <103083880+franciscourquiza@users.noreply.github.com>
2022-10-03 22:04:01 -0300

    Add files via upload

---
+++ exercises/exercise3.py
@@ -0,0 +1,95 @@
+"""Properties"""
+
+
+class Article:
+    """Re-Escribir el ejercicio anterior utilizando una property en vez de un
+    método de instancia.
+    Restricciones:
+        - Utilizar 3 variables de instancia
+        - Utilizar 1 property
+        - Utilizar 1 variable de clase
+        - Utilizar 1 método de clase
+        - No utilizar métodos de instancia
+        - No utilizar Dataclasses
+        - Utilizar Type Hints en todos los métodos y variables
+    """
+    __iva = 0.21
+
+    def __init__(self, nombre, costo, descuento = 0):
+        self.nombre = nombre
+        self.costo = costo 
+        self.descuento = descuento
+
+    @property 
+    def precio(self):
+        precio =round(self.costo + (self.costo * self.__iva),2)
+        precio_fin = round(precio-(self.descuento * precio),2)
+        return precio_fin
+
+    @classmethod  
+    def actualizar_iva(cls, nuevo_iva): 
+        cls.__iva = nuevo_iva
+        return cls.__iva
+
+    def calcular_precio(self):
+        precio_iva = round((self.costo * self.__iva) + self.costo,2)
+        precio = round((precio_iva - precio_iva * self.descuento),2)
+        return precio
+
+
+# NO MODIFICAR - INICIO
+# Test parámetro obligatorio
+try:
+    article = Article()
+    assert False, "No se puede instanciar sin nombre ni costo"
+except TypeError:
+    assert True
+
+try:
+    article = Article("Auto")
+    assert False, "No se puede instanciar sin costo"
+except TypeError:
+    assert True
+
+try:
+    article = Article(nombre="Auto", costo=1)
+    assert True
+except TypeError:
+    assert False, "El descuento es opcional"
+
+try:
+    article = Article(nombre="Auto", costo=1)
+    article.precio = 2
+    assert False, "No se puede modificar el precio"
+except AttributeError:
+    assert True
+
+
+# Test básico
+article = Article("Auto", 1)
+assert article.nombre == "Auto"
+assert article.precio == 1.21
+
+
+article = Article("Auto", 1, 0.21)
+assert article.nombre == "Auto"
+assert article.precio == 0.96
+
+
+# Test palabra clave
+article = Article(costo=1, nombre="Auto")
+assert article.nombre == "Auto"
+assert article.precio == 1.21
+
+article = Article(costo=1, nombre="Auto", descuento=0.21)
+assert article.nombre == "Auto"
+assert article.precio == 0.96
+
+
+# Test de método de clase
+Article.actualizar_iva(0.25)
+
+article = Article(costo=1, nombre="Auto")
+assert article.nombre == "Auto"
+assert article.precio == 1.25
+# NO MODIFICAR - FIN
